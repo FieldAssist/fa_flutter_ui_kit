092e2fb7ba2f003a8b26529e569af646edff51bb
Aashish <aashishc901@gmail.com>
2021-04-20 14:08:00 +0530

    [UI Kit] Misc
    Signed-off-by: Aashish <aashishc901@gmail.com>

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:fa_flutter_ui_kit/fa_flutter_ui_kit.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await AppConfig.instance.initiliaze();
   runApp(MyApp());
 }
 
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FALoginPage(),
     );
   }
 }
