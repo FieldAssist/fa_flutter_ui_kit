db7c8f7ffa8f78558e2658f1880eed097bbaf5bf
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-04-23 15:55:33 +0530

    Fix test cases
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- lib/fa_flutter_ui_kit.dart
+++ lib/fa_flutter_ui_kit.dart
@@ -1,17 +1,11 @@
 library fa_flutter_ui_kit;
 
-//Initialiaze config file to use the ui-kit
+//Initialize config file to use the ui-kit
 export 'src/config/index.dart';
 export 'src/constants/index.dart';
 export 'src/core/index.dart';
 export 'src/data/index.dart';
-export 'src/navigator/index.dart';
 export 'src/modules/index.dart';
+export 'src/navigator/index.dart';
 export 'src/utils/index.dart';
 export 'src/widgets/index.dart';
-
-/// A Calculator
-class Calculator {
-  ///Returns [value] plus 1
-  int addOne(int value) => value + 1;
-}
---
+++ test/sample_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('This is a sample GET-WHEN-THEN test', () {
+    const value = 5;
+    expect(value == 5, true);
+  });
+}
