187608b9e6cdf3073fa156337fb0c8d5f25b0d78
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-09-13 16:36:54 +0530

    [Pubspec] Upgrade packages to latest
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import geolocator_apple
 import package_info
 import path_provider_macos
 import shared_preferences_macos
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
