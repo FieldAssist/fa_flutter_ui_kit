7bfd09ef7374a418ba11b9903cd593d9e0860a47
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-12-05 08:35:39 +0530

    [Pubspec] Upgrade geolocation package to latest
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import geolocator_apple
+import package_info
+import path_provider_macos
+import shared_preferences_macos
+import sqflite
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+}
