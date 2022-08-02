5a4c4a863fff9417305700c71fc5eb6196da3dd3
Ritesh Khadse <r.khadse@flick2know.com>
2022-08-02 17:07:54 +0530

    [Qr-code-scanner] extract upi id when available
    Signed-off-by: Ritesh Khadse <r.khadse@flick2know.com>

--- lib/src/widgets/qr_code_scanner/qr_code_scanner.dart
+++ lib/src/widgets/qr_code_scanner/qr_code_scanner.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
+import '../../utils/log_utils.dart';
+
 class QrCodeScanner extends StatefulWidget {
   const QrCodeScanner({Key? key}) : super(key: key);
 
@@ -14,6 +16,8 @@ class _QrScannerState extends State<QrCodeScanner> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   Barcode? result;
   QRViewController? controller;
+  bool containsUPI = false;
+  final upiRegx = RegExp("[a-zA-Z0-9.\-_]{2,256}@[a-zA-Z]{2,64}");
 
   @override
   void reassemble() {
@@ -56,7 +60,10 @@ class _QrScannerState extends State<QrCodeScanner> {
           if (result != null)
             Expanded(
               flex: 1,
-              child: Text(result!.code ?? ''),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(result!.code ?? ''),
+              ),
             ),
           Expanded(
             flex: 2,
@@ -78,9 +85,7 @@ class _QrScannerState extends State<QrCodeScanner> {
                 ),
                 if (result != null)
                   IconButton(
-                    onPressed: () {
-                      Navigator.pop(context, result?.code);
-                    },
+                    onPressed: onPressSubmit,
                     color: Colors.green,
                     iconSize: 35,
                     icon: Icon(Icons.check_rounded),
@@ -91,6 +96,27 @@ class _QrScannerState extends State<QrCodeScanner> {
         ],
       ),
     );
+  }
+
+  void onPressSubmit() {
+    if (result != null) {
+      String? dataToReturn;
+      final scannedData = result!.code;
+      if (scannedData != null && upiRegx.hasMatch(scannedData)) {
+        dataToReturn = _extractUPIID(scannedData);
+      } else if (scannedData != null) {
+        dataToReturn = scannedData;
+      } else {
+        throw Exception("Scanned data is empty");
+      }
+      Navigator.pop(context, dataToReturn);
+    }
+  }
+
+  String _extractUPIID(String scannedData) {
+    final startIndex = scannedData.indexOf('?pa=') + 4;
+    final endIndex = scannedData.indexOf('&pn=');
+    return scannedData.substring(startIndex, endIndex);
   }
 
   @override
