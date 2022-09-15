2e539b90ec3c25766819371bea5dcf8a2e0c12bf
Taranjeet Singh <taranjeet@flick2know.com>
2022-09-15 12:32:34 +0530

    [Flavor] Add other debug flavor
    Signed-off-by: Taranjeet Singh <taranjeet@flick2know.com>

--- example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/raghavpawar/MobileDev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/raghavpawar/Desktop/Coding/Flutter/fa_flutter_ui_kit/example"
+export "FLUTTER_ROOT=/Users/flick2know-mac/Documents/src/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/flick2know-mac/Documents/fieldassist/fa_flutter_ui_kit/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
