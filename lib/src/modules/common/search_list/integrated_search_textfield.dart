08422f53b242aec5a33ed751f457de6e65db0f5a
Khushboo1702 <khushboo.agrawal@flick2know.com>
2023-04-10 19:30:38 +0530

    [IntegratedSearchTextField] fixing mic render flow

--- lib/src/modules/common/search_list/integrated_search_textfield.dart
+++ lib/src/modules/common/search_list/integrated_search_textfield.dart
@@ -3,21 +3,21 @@ import 'package:flutter_tts/flutter_tts.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 class IntegratedSearchTextField extends StatefulWidget {
-  const IntegratedSearchTextField(
-      {required this.searchFieldLabel,
-      this.keyboardType,
-      this.textInputAction,
-      this.onSubmitted,
-      this.prefixIcon,
-      this.bgColor,
-      this.searchThroughMic = false,
-      this.onTap,
-      this.elevation,
-      this.autoFocus,
-      required this.queryTextController,
-      this.showCrossbutton = false,
-      Key? key})
-      : super(key: key);
+  const IntegratedSearchTextField({
+    required this.searchFieldLabel,
+    this.keyboardType,
+    this.textInputAction,
+    this.onSubmitted,
+    this.prefixIcon,
+    this.bgColor,
+    this.onTap,
+    this.elevation,
+    this.autoFocus,
+    this.searchThroughMic = false,
+    required this.queryTextController,
+    this.showCrossbutton = false,
+    Key? key,
+  }) : super(key: key);
 
   final double? elevation;
   final Color? bgColor;
@@ -43,90 +43,93 @@ class _IntegratedSearchTextFieldState extends State<IntegratedSearchTextField> {
   stt.SpeechToText speech = stt.SpeechToText();
   String recognizedText = '';
   FlutterTts _flutterTts = FlutterTts();
-
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Card(
         elevation: widget.elevation ?? 0,
-        child: Row(
-          children: <Widget>[
-            TextField(
-              onTap: widget.onTap,
-              controller: widget.queryTextController,
-              style: TextStyle(color: Colors.black),
-              autofocus: widget.autoFocus ?? true,
-              textInputAction: widget.textInputAction,
-              keyboardType: widget.keyboardType,
-              onSubmitted: widget.onSubmitted,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: widget.bgColor,
-                prefixIcon: widget.prefixIcon,
-                isDense: true,
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                contentPadding: const EdgeInsets.symmetric(
-                  horizontal: 16,
-                  vertical: 8,
-                ),
-                hintText: widget.searchFieldLabel,
-                suffixIcon: widget.showCrossbutton
-                    ? Container(
-                        width: 18,
-                        height: 18,
-                        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(9),
-                          color: Colors.black.withOpacity(0.4),
-                        ),
-                        child: InkWell(
-                          onTap: widget.queryTextController.clear,
-                          child: Icon(
-                            Icons.close,
-                            size: 12,
-                            color: Colors.white,
-                          ),
-                        ),
-                      )
-                    : null,
-                suffixIconConstraints: BoxConstraints(
-                  maxHeight: 38,
-                  maxWidth: 38,
-                ),
-                hintStyle: TextStyle(
-                  color: Colors.black26,
-                ),
-              ),
+        child: TextField(
+          onTap: widget.onTap,
+          controller: widget.queryTextController,
+          style: TextStyle(color: Colors.black),
+          autofocus: widget.autoFocus ?? true,
+          textInputAction: widget.textInputAction,
+          keyboardType: widget.keyboardType,
+          onSubmitted: widget.onSubmitted,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: widget.bgColor,
+            prefixIcon: widget.prefixIcon,
+            isDense: true,
+            border: OutlineInputBorder(
+              borderSide: BorderSide.none,
+              borderRadius: BorderRadius.circular(5),
             ),
-            if (widget.searchThroughMic)
-              IconButton(
-                onPressed: () async {
-                  await _speakPrompt();
-                  await Future.delayed(
-                    const Duration(seconds: 1),
-                  );
-                  var available = await speech.initialize();
-                  if (available) {
-                    await speech.listen(
-                      onResult: (result) {
-                        setState(() {
-                          recognizedText = result.recognizedWords;
-                          widget.queryTextController.text = recognizedText;
-                        });
-                        if (result.finalResult) {
-                          speech.stop();
+            contentPadding: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+            hintText: widget.searchFieldLabel,
+            suffixIcon: Row(
+              children: <Widget>[
+                if (widget.searchThroughMic)
+                  SizedBox(
+                    width: 20,
+                    child: IconButton(
+                      onPressed: () async {
+                        await _speakPrompt();
+                        await Future.delayed(
+                          const Duration(seconds: 1),
+                        );
+                        var available = await speech.initialize();
+                        if (available) {
+                          await speech.listen(
+                            onResult: (result) {
+                              setState(() {
+                                print(widget.queryTextController.text);
+                                recognizedText = result.recognizedWords;
+                                widget.queryTextController.text =
+                                    recognizedText;
+                              });
+                              if (result.finalResult) {
+                                speech.stop();
+                              }
+                            },
+                          );
                         }
                       },
-                    );
-                  }
-                },
-                icon: Icon(Icons.mic),
-              ),
-          ],
+                      icon: Icon(Icons.mic),
+                    ),
+                  ),
+                if (widget.showCrossbutton)
+                  Container(
+                    width: 18,
+                    height: 18,
+                    margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(9),
+                      color: Colors.black.withOpacity(0.4),
+                    ),
+                    child: InkWell(
+                      onTap: widget.queryTextController.clear,
+                      child: Icon(
+                        Icons.close,
+                        size: 12,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+            suffixIconConstraints: BoxConstraints(
+              maxHeight: 38,
+              maxWidth: 38,
+            ),
+            hintStyle: TextStyle(
+              color: Colors.black26,
+            ),
+          ),
         ),
       ),
     );
