95bb72b99f48ada0d4a28a8f268e933c6964b62e
Dev <devbindal2003@gmail.com>
2024-03-15 13:51:13 +0530

    fix(multi-select): change background color if disabled

--- lib/src/widgets/common/multi_select_items/bottom_sheet/multi_select_bottom_sheet_field.dart
+++ lib/src/widgets/common/multi_select_items/bottom_sheet/multi_select_bottom_sheet_field.dart
@@ -356,11 +356,13 @@ class __MultiSelectBottomSheetFieldViewState<V>
             }
           },
           decoration: widget.chipDisplay!.decoration,
-          chipColor: widget.chipDisplay!.chipColor ??
-              ((widget.selectedColor != null &&
-                      widget.selectedColor != Colors.transparent)
-                  ? widget.selectedColor!.withOpacity(0.35)
-                  : null),
+          chipColor: !widget.enabled
+              ? Colors.grey
+              : widget.chipDisplay!.chipColor ??
+                  ((widget.selectedColor != null &&
+                          widget.selectedColor != Colors.transparent)
+                      ? widget.selectedColor!.withOpacity(0.35)
+                      : null),
           alignment: widget.chipDisplay!.alignment,
           textStyle: widget.chipDisplay!.textStyle,
           icon: widget.chipDisplay!.icon,
@@ -450,15 +452,17 @@ class __MultiSelectBottomSheetFieldViewState<V>
                     BoxDecoration(
                       border: Border(
                         bottom: BorderSide(
-                          color: widget.state != null && widget.state!.hasError
-                              ? Colors.red.shade800.withOpacity(0.6)
-                              : _selectedItems.isNotEmpty
-                                  ? (widget.selectedColor != null &&
-                                          widget.selectedColor !=
-                                              Colors.transparent)
-                                      ? widget.selectedColor!
-                                      : Theme.of(context).primaryColor
-                                  : Colors.black45,
+                          color: !widget.enabled
+                              ? Colors.grey
+                              : widget.state != null && widget.state!.hasError
+                                  ? Colors.red.shade800.withOpacity(0.6)
+                                  : _selectedItems.isNotEmpty
+                                      ? (widget.selectedColor != null &&
+                                              widget.selectedColor !=
+                                                  Colors.transparent)
+                                          ? widget.selectedColor!
+                                          : Theme.of(context).primaryColor
+                                      : Colors.black45,
                           width: _selectedItems.isNotEmpty
                               ? (widget.state != null && widget.state!.hasError)
                                   ? 1.4
