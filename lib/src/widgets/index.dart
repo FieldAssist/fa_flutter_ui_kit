e9ce491257ba7289de46443321e0f55dac8c8381
Ritesh Khadse <r.khadse@flick2know.com>
2024-04-03 12:33:59 +0530

    [order] add order summary widgets
    Signed-off-by: Ritesh Khadse <r.khadse@flick2know.com>

--- lib/src/widgets/index.dart
+++ lib/src/widgets/index.dart
@@ -1,4 +1,5 @@
 export 'common/index.dart';
+export 'order_booking/index.dart';
 export 'qr_code_scanner/qr_code_scanner.dart';
-export 'searchable_drop_down/searchable_drop_down.dart';
 export 'searchable_drop_down/searchable_bottom_sheet/searchable_bottom_sheet.dart';
+export 'searchable_drop_down/searchable_drop_down.dart';
---
+++ lib/src/widgets/order_booking/index.dart
@@ -0,0 +1,2 @@
+export '../order_booking/summary/order_amount_summary.dart';
+export '../order_booking/summary/product_summary_card.dart';
