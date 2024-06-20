398949f79fd2d9a016111b5e50cff41ee833ee45
Khushboo1702 <khushboo.agrawal@flick2know.com>
2024-06-20 10:56:23 +0530

    fix(file-upload): implement static functions

--- lib/src/modules/common/file_upload/file_uploader.dart
+++ lib/src/modules/common/file_upload/file_uploader.dart
@@ -3,31 +3,36 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:open_file/src/platform/open_file.dart';
 
-Future<PlatformFile?> getImage(ImageSource source, StateSetter setState) async {
-  final pickedFile = await ImagePicker().pickImage(source: source);
+class FetchFile {
+  FetchFile._();
 
-  if (pickedFile != null) {
-    return PlatformFile(
-      name: "uploaded_file",
-      size: 5,
-      path: pickedFile.path,
+  static Future<PlatformFile?> getImage(
+      ImageSource source, StateSetter setState) async {
+    final pickedFile = await ImagePicker().pickImage(source: source);
+
+    if (pickedFile != null) {
+      return PlatformFile(
+        name: "uploaded_file",
+        size: 5,
+        path: pickedFile.path,
+      );
+    }
+    return null;
+  }
+
+  static Future<PlatformFile?> getDocument(StateSetter setState) async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf', 'jpg', 'jpeg', 'png', 'gif'],
     );
+
+    if (result != null) {
+      return result.files.first;
+    }
+    return null;
   }
-  return null;
-}
 
-Future<PlatformFile?> getDocument(StateSetter setState) async {
-  FilePickerResult? result = await FilePicker.platform.pickFiles(
-    type: FileType.custom,
-    allowedExtensions: ['pdf', 'jpg', 'jpeg', 'png', 'gif'],
-  );
-
-  if (result != null) {
-    return result.files.first;
+  static void viewFile(PlatformFile _image) {
+    OpenFile.open(_image.path);
   }
-  return null;
-}
-
-void viewFile(PlatformFile _image) {
-  OpenFile.open(_image.path);
 }
