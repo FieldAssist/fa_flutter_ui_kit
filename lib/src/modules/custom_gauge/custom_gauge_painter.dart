c8989da92e0e278fb849489372aafd85e3cd17b8
Khushboo1702 <khushboo.agrawal@flick2know.com>
2024-07-17 10:40:40 +0530

    temp commit2

--- lib/src/modules/custom_gauge/custom_gauge_painter.dart
+++ lib/src/modules/custom_gauge/custom_gauge_painter.dart
@@ -13,17 +13,24 @@ class CustomGaugePainter extends CustomPainter {
   final double targetValue;
   final Color backgroundColor;
 
-  Color getGaugeColor(double value, double target) {
+  Shader getGradientShader(Rect rect, double value, double target) {
     final percentage = (value / target) * 100;
+    List<Color> colors;
+
     if (percentage <= 25) {
-      return Colors.red;
+      colors = [Colors.red, Colors.orange];
     } else if (percentage <= 50) {
-      return Colors.orange;
+      colors = [Colors.orange, Colors.yellow];
     } else if (percentage <= 75) {
-      return Colors.yellow;
+      colors = [Colors.yellow, Colors.green];
     } else {
-      return Colors.green;
+      colors = [Colors.green, Colors.greenAccent];
     }
+
+    return LinearGradient(
+      colors: colors,
+      stops: [0.0, 1.0],
+    ).createShader(rect);
   }
 
   @override
@@ -34,6 +41,7 @@ class CustomGaugePainter extends CustomPainter {
 
     final center = Offset(size.width / 2, size.height);
 
+    // Background arc
     paint.color = backgroundColor;
     canvas.drawArc(
       Rect.fromCircle(center: center, radius: 100),
@@ -43,46 +51,39 @@ class CustomGaugePainter extends CustomPainter {
       paint,
     );
 
-    paint.color = getGaugeColor(currentValue, targetValue);
-    final sweepAngle =
-        (pi * (targetValue < currentValue ? targetValue : currentValue)) /
-            targetValue;
+    // Gauge arc
+    final sweepAngle = (pi * (targetValue < currentValue ? targetValue : currentValue)) / targetValue;
+    final rect = Rect.fromCircle(center: center, radius: 100);
+    paint.shader = getGradientShader(rect, currentValue, targetValue);
     canvas.drawArc(
-      Rect.fromCircle(center: center, radius: 100),
+      rect,
       pi,
       sweepAngle,
       false,
       paint,
     );
 
+    // Start label
     final startLabelPainter = TextPainter(
-      text: TextSpan(
-          text: '0', style: TextStyle(color: Colors.black, fontSize: 16)),
+      text: TextSpan(text: '0', style: TextStyle(color: Colors.black, fontSize: 16)),
       textDirection: TextDirection.ltr,
     );
     startLabelPainter
       ..layout()
       ..paint(canvas, Offset(center.dx - 110, size.height + 10));
 
+    // Target label
     final targetLabelPainter = TextPainter(
-      text: TextSpan(
-        text: targetValue.toString(),
-        style: TextStyle(color: Colors.black, fontSize: 16),
-      ),
+      text: TextSpan(text: targetValue.toString(), style: TextStyle(color: Colors.black, fontSize: 16)),
       textDirection: TextDirection.ltr,
     );
     targetLabelPainter.layout();
     targetLabelPainter.paint(canvas, Offset(center.dx + 80, size.height + 10));
 
+    // Current value
     final currentValueText = currentValue.toStringAsFixed(1);
     final currentValuePainter = TextPainter(
-      text: TextSpan(
-        text: currentValueText,
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 16,
-        ),
-      ),
+      text: TextSpan(text: currentValueText, style: TextStyle(color: Colors.black, fontSize: 16)),
       textDirection: TextDirection.ltr,
     );
     currentValuePainter.layout();
