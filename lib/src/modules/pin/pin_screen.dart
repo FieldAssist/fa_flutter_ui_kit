3014e7a8a56e2d96eb595f975e2972054035ca9b
Taranjeet Singh <taranjeet@flick2know.com>
2024-09-24 13:42:04 +0530

    feat(country-picker): implement selected country builder functionality
    Signed-off-by: Taranjeet Singh <taranjeet@flick2know.com>

--- lib/src/modules/pin/pin_screen.dart
+++ lib/src/modules/pin/pin_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
+// import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
 
 class PinScreen extends StatefulWidget {
   const PinScreen({super.key});
@@ -13,16 +13,16 @@ class _PinScreenState extends State<PinScreen> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        OtpTextField(
-          obscureText: true,
-          numberOfFields: 4,
-          borderColor: Colors.grey,
-          focusedBorderColor: Colors.black,
-          showFieldAsBox: false,
-          borderWidth: 4.0,
-          onCodeChanged: (String code) {},
-          onSubmit: (code) {},
-        ),
+        // OtpTextField(
+        //   obscureText: true,
+        //   numberOfFields: 4,
+        //   borderColor: Colors.grey,
+        //   focusedBorderColor: Colors.black,
+        //   showFieldAsBox: false,
+        //   borderWidth: 4.0,
+        //   onCodeChanged: (String code) {},
+        //   onSubmit: (code) {},
+        // ),
       ],
     );
   }
