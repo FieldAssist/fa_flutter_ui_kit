252168e2ba08cc536577b1a446a9a9a5668a720a
Ritesh Khadse <r.khadse@flick2know.com>
2024-09-30 18:43:13 +0530

    [order-summary] add currency utils and its usages
    Signed-off-by: Ritesh Khadse <r.khadse@flick2know.com>

---
+++ lib/src/utils/currency_utils.dart
@@ -0,0 +1,36 @@
+import 'package:intl/intl.dart';
+
+class CurrencyUtil {
+  CurrencyUtil._();
+
+  static final NumberFormat _indCurrencyDoubleFormat =
+      NumberFormat("##,##,###.##", "en_IN");
+
+  static final NumberFormat _indCurrencyIntFormat =
+      NumberFormat.simpleCurrency(decimalDigits: 2, locale: "en_IN");
+
+  static final NumberFormat _indCurrencyDouble2PlacesFormat =
+      NumberFormat("##,##,###.00", "en_IN");
+
+  static String getFormattedInrDouble(num amount) =>
+      _indCurrencyDoubleFormat.format(amount);
+
+  static String getFormattedInrInt(num amount) =>
+      _indCurrencyIntFormat.format(amount);
+
+  static String getFormattedIntDouble2Places(num amount) =>
+      _indCurrencyDouble2PlacesFormat.format(amount);
+
+//TEMP
+  static String convertToIndianNumbering(int number) {
+    if (number < 1000) {
+      return number.toString();
+    } else if (number < 100000) {
+      return '${(number / 1000).toStringAsFixed(0)}K';
+    } else if (number < 10000000) {
+      return '${(number / 100000).toStringAsFixed(0)} Lakhs';
+    } else {
+      return '${(number / 10000000).toStringAsFixed(2)}Cr';
+    }
+  }
+}
