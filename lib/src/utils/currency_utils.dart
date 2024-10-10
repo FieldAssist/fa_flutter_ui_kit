3c09e76acf2e1118f0d4f95afa9111de25673e9f
sourav <sourav.sharma@flick2know.com>
2024-10-10 15:49:41 +0530

    feat(currency-utils): add international number system
    https://app.asana.com/0/1203045279492787/1208396483431298/f
    Signed-off-by: sourav <sourav.sharma@flick2know.com>

--- lib/src/utils/currency_utils.dart
+++ lib/src/utils/currency_utils.dart
@@ -1,35 +1,65 @@
 import 'package:intl/intl.dart';
 
 class CurrencyUtil {
-  CurrencyUtil._();
+  bool isInternationalCompany;
 
-  static final NumberFormat _indCurrencyDoubleFormat =
+  CurrencyUtil({this.isInternationalCompany = false});
+
+  final NumberFormat _indCurrencyDoubleFormat =
       NumberFormat("##,##,###.##", "en_IN");
 
-  static final NumberFormat _indCurrencyIntFormat =
+  final NumberFormat _indCurrencyIntFormat =
       NumberFormat.simpleCurrency(decimalDigits: 2, locale: "en_IN");
 
-  static final NumberFormat _indCurrencyDouble2PlacesFormat =
+  final NumberFormat _internationalCurrencyIntFormat =
+      NumberFormat.simpleCurrency(decimalDigits: 1, locale: "en_US");
+
+  final NumberFormat _indCurrencyDouble2PlacesFormat =
       NumberFormat("##,##,###.00", "en_IN");
 
-  static String getFormattedInrDouble(num amount) =>
-      _indCurrencyDoubleFormat.format(amount);
+  String getFormattedInrDouble(num amount) => isInternationalCompany
+      ? convertToInternationalNumbering(amount.toDouble())
+      : _indCurrencyDoubleFormat.format(amount);
 
-  static String getFormattedInrInt(num amount) =>
-      _indCurrencyIntFormat.format(amount);
+  String getFormattedInrInt(num amount) => isInternationalCompany
+      ? _internationalCurrencyIntFormat.format(amount)
+      : _indCurrencyIntFormat.format(amount);
 
-  static String getFormattedIntDouble2Places(num amount) =>
-      _indCurrencyDouble2PlacesFormat.format(amount);
-  
-  static String formatNumber(num amt) {
-    final number = (amt * 100).round() / 100;
-    final formatter = NumberFormat.currency(
-      decimalDigits: 2,
-      locale: "en_IN",
-      symbol: "",
-    );
+  String getFormattedIntDouble2Places(num amount) => isInternationalCompany
+      ? convertToInternationalNumbering(amount.toDouble(), decimalPlaces: 2)
+      : _indCurrencyDouble2PlacesFormat.format(amount);
 
-    return formatter.format(number);
+  String convertToInternationalNumbering(double value,
+      {int decimalPlaces = 1, isCompact = true}) {
+    String _getHashForDecimalPlaces(int decimalPlaces) {
+      var hash = "";
+      for (var x = 0; x < decimalPlaces - 1; x++) {
+        hash = hash + "#";
+      }
+      return hash;
+    }
+
+    if (!isCompact) {
+      if (decimalPlaces > 0) {
+        final formatter =
+            NumberFormat("###,###.0${_getHashForDecimalPlaces(decimalPlaces)}");
+        return formatter.format(value);
+      } else {
+        final formatter = NumberFormat("###,###");
+        return formatter.format(value);
+      }
+    }
+
+    if (value > 1000000000) {
+      final calculated = value / 1000000000;
+      return '${calculated.toStringAsFixed(decimalPlaces)} B';
+    } else if (value > 1000000) {
+      final calculated = value / 1000000;
+      return '${calculated.toStringAsFixed(decimalPlaces)} M';
+    } else {
+      final numberFormat = NumberFormat("#,##0.0", "en_US");
+      return numberFormat.format(value);
+    }
   }
 
 //TEMP
