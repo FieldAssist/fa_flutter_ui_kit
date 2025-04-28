511830f82d12486921fc7f0ea6145fa088d20224
Vineet Singh <vineet@Vineets-MacBook-Air.local>
2025-04-28 14:06:08 +0530

    feat(safe-device):restrict user to use fake locataion,timeZone and time

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import app_settings
 import device_info_plus
 import file_selector_macos
 import firebase_analytics
@@ -22,6 +23,7 @@ import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
---
+++ lib/src/app/location_time_helper.dart
@@ -0,0 +1,52 @@
+import 'package:fa_flutter_ui_kit/src/utils/log_utils.dart';
+import 'package:flutter/services.dart';
+import 'package:safe_device/safe_device.dart';
+
+class DeviceTrustChecker {
+  static const MethodChannel _channel = MethodChannel("GT_APP_CHANNEL");
+  Future<bool> checkIfDeveloperModeEnabled() async {
+    final isMockedLocation = await SafeDevice.canMockLocation;
+    final isDevelopmentMode = await SafeDevice.isDevelopmentModeEnable;
+    return isMockedLocation || isDevelopmentMode;
+  }
+
+  Future<bool> checkIfAutoTimeZoneEnabled() async {
+    try {
+      final isAutoTimeZoneEnabled =
+      await _channel.invokeMethod("isAutoTimeZoneEnabled");
+      logger.i("Auto time zone enabled: $isAutoTimeZoneEnabled");
+      return isAutoTimeZoneEnabled;
+    } on PlatformException catch (e, s) {
+      logger.e(e, s);
+      return false;
+    } catch (e, s) {
+      logger.e(e, s);
+      return false;
+    }
+  }
+
+  Future<bool> checkIfAutoTimeEnabled() async {
+    try {
+      final isAutoTimeEnabled =
+      await _channel.invokeMethod("isAutoTimeEnabled");
+      logger.i("Auto time enabled: $isAutoTimeEnabled");
+      return isAutoTimeEnabled;
+    } on PlatformException catch (e, s) {
+      logger.e(e, s);
+      return false;
+    } catch (e, s) {
+      logger.e(e, s);
+      return false;
+    }
+  }
+
+  // Future<void> checkIfProceed() async {
+  //   isDeveloperMode = await checkIfDeveloperModeEnabled();
+  //   isAutoTimeZoneEnabled = await checkIfAutoTimeZoneEnabled();
+  //   isAutoTimeEnabled = await checkIfAutoTimeEnabled();
+  //   _subject
+  //       .add(!isDeveloperMode && isAutoTimeZoneEnabled && isAutoTimeEnabled);
+  //   logger.i(
+  //       "Device trust status - Developer Mode: $isDeveloperMode, Auto Time Zone is enabled: $isAutoTimeZoneEnabled, Auto Time is enabled: $isAutoTimeEnabled");
+  // }
+}
