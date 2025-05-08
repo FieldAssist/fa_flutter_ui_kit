a08d5f82702b4bb29b2c76d2b51a4bf3f4825ed4
Dev Bindal <81467330+devbindal@users.noreply.github.com>
2025-05-08 18:59:37 +0530

    [version] upgrade flutter & dependencies (#137)
    * [version] upgrade flutter &
    dependencies
    
    * chore: comment mobile_number_picker
    
    Signed-off-by: kapdroid <kapilsharma130519@gmail.com>
    
    * upgrade dependencies
    
    Signed-off-by: riteshkhadse <r.khadse@flick2know.com>
    
    * refactor: remove mobile_number_picker and open_file
    
    Signed-off-by: Vatsal Gandhi <gandhivatsal17@gmail.com>
    
    * chore : update dart core and flutter core ref.
    
    Signed-off-by: TusharFA <tushar@flick2know.com>
    
    ---------
    
    Signed-off-by: kapdroid <kapilsharma130519@gmail.com>
    Signed-off-by: riteshkhadse <r.khadse@flick2know.com>
    Signed-off-by: Vatsal Gandhi <gandhivatsal17@gmail.com>
    Signed-off-by: TusharFA <tushar@flick2know.com>
    Co-authored-by: kapdroid <kapilsharma130519@gmail.com>
    Co-authored-by: riteshkhadse <r.khadse@flick2know.com>
    Co-authored-by: Vatsal Gandhi <gandhivatsal17@gmail.com>
    Co-authored-by: TusharFA <tushar@flick2know.com>

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,36 +5,42 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
 import device_info_plus
+import file_picker
 import file_selector_macos
 import firebase_analytics
 import firebase_core
 import firebase_crashlytics
 import firebase_messaging
+import flutter_image_compress_macos
 import flutter_tts
 import geolocator_apple
-import open_file_mac
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
-import speech_to_text_macos
-import sqflite
+import speech_to_text
+import sqflite_darwin
 import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
