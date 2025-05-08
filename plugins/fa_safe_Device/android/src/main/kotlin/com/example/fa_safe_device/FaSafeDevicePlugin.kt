23ea49f15a622ab65e14300594da9c8c8417b3c1
Vineet Singh <vineet@Vineets-MacBook-Air.local>
2025-05-08 12:50:42 +0530

    feat(plugins/safe-device):timeZone,autoTime and developer mode check

---
+++ plugins/fa_safe_Device/android/src/main/kotlin/com/example/fa_safe_device/FaSafeDevicePlugin.kt
@@ -0,0 +1,50 @@
+package com.example.fa_safe_device
+
+import androidx.annotation.NonNull
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+
+/** FaSafeDevicePlugin */
+class FaSafeDevicePlugin: FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
+
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "fa_safe_device")
+    channel.setMethodCallHandler(this)
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else if(call.method=="isAutoTimeZoneEnabled"){
+      val isAutoTimeZoneEnabled = android.provider.Settings.Global.getInt(
+        contentResolver,
+        android.provider.Settings.Global.AUTO_TIME_ZONE,
+        0
+      ) == 1
+      result.success(isAutoTimeZoneEnabled)
+    }else if(call.method=="isAutoTimeEnabled"){
+      val isAutoTimeEnabled = android.provider.Settings.Global.getInt(
+        contentResolver,
+        android.provider.Settings.Global.AUTO_TIME,
+        0
+      ) == 1
+      result.success(isAutoTimeEnabled)
+    }
+    else {
+      result.notImplemented()
+    }
+  }
+
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+}
---
+++ plugins/fa_safe_Device/lib/fa_safe_device_method_channel.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:safe_device/safe_device.dart';
+import 'fa_safe_device_platform_interface.dart';
+
+/// An implementation of [FaSafeDevicePlatform] that uses method channels.
+class MethodChannelFaSafeDevice extends FaSafeDevicePlatform {
+  /// The method channel used to interact with the native platform.
+  @visibleForTesting
+  final methodChannel = const MethodChannel('fa_safe_device');
+
+  @override
+  Future<String?> getPlatformVersion() async {
+    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    return version;
+  }
+
+  @override
+  Future<bool> isAutoTimeZoneEnabled() async {
+    final isAutoTimeZoneEnabled = await methodChannel.invokeMethod<bool>('isAutoTimeZoneEnabled');
+    return isAutoTimeZoneEnabled ?? false;
+  }
+
+  @override
+  Future<bool> isAutoTimeEnabled() async {
+    final isAutoTimeEnabled = await methodChannel.invokeMethod<bool>('isAutoTimeEnabled');
+    return isAutoTimeEnabled ?? false;
+  }
+
+  @override
+  Future<bool> isDeveloperModeEnabled() async {
+    final isDeveloperModeEnabled = await SafeDevice.isDevelopmentModeEnable && await SafeDevice.isMockLocation;
+    return isDeveloperModeEnabled;
+  }
+
+}
