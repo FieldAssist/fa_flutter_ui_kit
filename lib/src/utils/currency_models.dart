40270147b859ee423ed9e791cac2300f3355fc2a
PrakharMittal26 <prakhar.mittal@flick2know.com>
2025-06-06 16:14:36 +0530

    refactor: separate currency enums and models into dedicated files
    (cherry picked from commit 5b6ff0066faac9adebe4469d74aef79dde515076)

---
+++ lib/src/utils/currency_models.dart
@@ -0,0 +1,23 @@
+import 'package:fa_flutter_ui_kit/src/utils/currency_enums.dart';
+
+class CompactScale {
+  const CompactScale({
+    required this.threshold,
+    required this.divisor,
+    required this.suffix,
+  });
+  final double threshold;
+  final double divisor;
+  final CurrencySuffix suffix;
+}
+
+class CompactFormatResult {
+  const CompactFormatResult({
+    required this.value,
+    required this.suffix,
+    required this.wasCompacted,
+  });
+  final double value;
+  final String suffix;
+  final bool wasCompacted;
+}
