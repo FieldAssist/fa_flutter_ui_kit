3a878c880c63ad9d5e3d9d6a02ebe47b4280c5ab
TusharFA <tushar@flick2know.com>
2025-06-16 14:17:27 +0530

    feat(permission) : Expose location permission and status function.
    Signed-off-by: TusharFA <tushar@flick2know.com>

--- lib/src/core/location_info/location_info.dart
+++ lib/src/core/location_info/location_info.dart
@@ -29,6 +29,10 @@ abstract class LocationInfo {
     LatLng inputLocation, {
     double minDistance = 300,
   });
+
+  Future<bool> isLocationEnabled();
+
+  Future<bool> isLocationPermissionGranted();
 }
 
 class LocationInfoImpl implements LocationInfo {
@@ -63,9 +67,9 @@ class LocationInfoImpl implements LocationInfo {
   @override
   Future initLocation() async {
     if (isMobile) {
-      final permissionStatus = await Geolocator.checkPermission();
+      final permissionStatus = await isLocationPermissionGranted();
 
-      if (!_isPermissionGranted(permissionStatus)) {
+      if (!permissionStatus) {
         if (navKey != null) {
           await DialogUtils.showAlertDialog(
               title: 'Location Permission Required!',
@@ -247,9 +251,9 @@ class LocationInfoImpl implements LocationInfo {
     if (!isTimerActive) {
       locationCheckTimer =
           Timer.periodic(const Duration(seconds: 4), (t) async {
-        final permission = await Geolocator.checkPermission();
-        final geolocationStatus = await Geolocator.isLocationServiceEnabled();
-        if (!_isPermissionGranted(permission) || !geolocationStatus) {
+        final _checkPermission = await isLocationPermissionGranted();
+        final geolocationStatus = await isLocationEnabled();
+        if (!_checkPermission || !geolocationStatus) {
           t.cancel();
           if (navKey != null) {
             await navKey!.currentState?.push(
@@ -364,6 +368,16 @@ class LocationInfoImpl implements LocationInfo {
       logger.e(e, s);
       return null;
     }
+  }
+
+  @override
+  Future<bool> isLocationEnabled() async =>
+      Geolocator.isLocationServiceEnabled();
+
+  @override
+  Future<bool> isLocationPermissionGranted() async {
+    final permission = await Geolocator.checkPermission();
+    return _isPermissionGranted(permission);
   }
 }
 
