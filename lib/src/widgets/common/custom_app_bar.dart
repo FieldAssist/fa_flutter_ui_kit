5db093bf8cd60e3c17d941bf7bdadf3840ee3576
unknown <aman.srivastava@flick2know.com>
2025-07-21 11:11:52 +0530

    fix/customAppBar: Rename

--- lib/src/widgets/common/custom_app_bar.dart
+++ lib/src/widgets/common/custom_app_bar.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show SystemUiOverlayStyle;
 
-class BaseCustomAppBar extends AppBar {
-  BaseCustomAppBar({
+class BaseAppBar extends AppBar {
+  BaseAppBar({
     Widget? leading,
     required Widget title,
     List<Widget>? actions,
