1faae57fa14262f523faafcc25de1ef57a50c1aa
Kapdroid <kapdroid@Kapdroids-MacBook-Pro.local>
2025-11-12 17:34:44 +0530

    chore: flutter upgrade to 3.35.7
    Signed-off-by: Kapdroid <kapdroid@Kapdroids-MacBook-Pro.local>

--- example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/tusharf2k/Downloads/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/tusharf2k/StudioProjects/fa_flutter_ui_kit/example"
+export "FLUTTER_ROOT=/Users/kapdroid/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/kapdroid/StudioProjects/fa_flutter_ui_kit/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
