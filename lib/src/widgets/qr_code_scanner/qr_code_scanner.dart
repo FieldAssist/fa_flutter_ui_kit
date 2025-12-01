b10ad9c646ea17fda7f5d42d4462e91fc02f4b24
Rajiv <rajiv@flick2know.com>
2025-12-01 14:02:34 +0530

    temp: remove qr scanner dependency

--- lib/src/widgets/qr_code_scanner/qr_code_scanner.dart
+++ lib/src/widgets/qr_code_scanner/qr_code_scanner.dart
@@ -1,186 +1,186 @@
-import 'dart:io';
-
-import 'package:fa_flutter_core/fa_flutter_core.dart';
-import 'package:flutter/material.dart';
-import 'package:qr_code_scanner_plus/qr_code_scanner_plus.dart';
-
-class QrCodeScanner extends StatefulWidget {
-  const QrCodeScanner({Key? key, this.validationRegex, this.scannedValues})
-      : super(key: key);
-
-  final String? validationRegex;
-  final List<String>? scannedValues;
-
-  @override
-  State<QrCodeScanner> createState() => _QrScannerState();
-}
-
-class _QrScannerState extends State<QrCodeScanner> {
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-  Barcode? result;
-  QRViewController? controller;
-  String? parsedData;
-  final upiRegx = RegExp(r'[a-zA-Z0-9.\-_]{2,256}@[a-zA-Z]{2,64}');
-  RegExp? validationRegex;
-
-  @override
-  void initState() {
-    super.initState();
-    if (widget.validationRegex != null) {
-      validationRegex = RegExp(widget.validationRegex!);
-    }
-  }
-
-  @override
-  void reassemble() {
-    super.reassemble();
-    if (Platform.isAndroid) {
-      controller!.pauseCamera();
-    } else if (Platform.isIOS) {
-      controller!.resumeCamera();
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 10,
-            child: QRView(
-              key: qrKey,
-              cameraFacing: CameraFacing.back,
-              overlay: QrScannerOverlayShape(
-                borderRadius: 12,
-                borderColor: Colors.green,
-                borderWidth: 10,
-                cutOutSize: 350,
-              ),
-              onQRViewCreated: (controller) {
-                if (this.controller == null) {
-                  setState(() {
-                    this.controller = controller;
-                  });
-                }
-
-                controller.resumeCamera(); //BUG
-
-                controller.scannedDataStream.listen((scanData) {
-                  if (scanData.code != null) {
-                    if (upiRegx.hasMatch(scanData.code!)) {
-                      parsedData = _extractUPIID(scanData.code!);
-                    } else {
-                      parsedData = scanData.code!;
-                    }
-                    result = scanData;
-                    setState(() {});
-                    controller.stopCamera();
-                  }
-                });
-              },
-            ),
-          ),
-          if (result != null)
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(parsedData ?? ''),
-              ),
-            ),
-          Expanded(
-            flex: 2,
-            child: Row(
-              mainAxisAlignment: result != null
-                  ? MainAxisAlignment.spaceAround
-                  : MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).clearSnackBars();
-                    if (result != null) {
-                      setState(() {
-                        result = null;
-                      });
-                      controller?.resumeCamera();
-                    } else {
-                      Navigator.pop(context);
-                    }
-                  },
-                  color: Colors.red,
-                  iconSize: 35,
-                  icon: Icon(Icons.close_outlined),
-                ),
-                if (result != null)
-                  IconButton(
-                    onPressed: onPressSubmit,
-                    color: Colors.green,
-                    iconSize: 35,
-                    icon: Icon(Icons.check_rounded),
-                  ),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  void onPressSubmit() {
-    if (result != null) {
-      String? dataToReturn;
-
-      if (widget.validationRegex != null &&
-          validationRegex != null &&
-          !validationRegex!.hasMatch(result!.code!)) {
-        ScaffoldMessenger.of(context).clearSnackBars();
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(
-              "Scanned QR code does not match the validation checks, Please try again.",
-            ),
-          ),
-        );
-        return;
-      }
-
-      if (parsedData != null) {
-        dataToReturn = parsedData;
-      } else if (result!.code != null) {
-        dataToReturn = result!.code!;
-      } else {
-        throw Exception("Scanned data is empty");
-      }
-
-      //incase of unique scans.
-      if (checkIfListIsNotEmpty(widget.scannedValues)) {
-        if (widget.scannedValues!.contains(dataToReturn)) {
-          ScaffoldMessenger.of(context).clearSnackBars();
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text(
-                "You have already scanned this QR code.",
-              ),
-            ),
-          );
-          return;
-        }
-      }
-
-      Navigator.pop(context, dataToReturn);
-    }
-  }
-
-  String _extractUPIID(String scannedData) {
-    final startIndex = scannedData.indexOf('?pa=') + 4;
-    final endIndex = scannedData.indexOf('&');
-    return scannedData.substring(startIndex, endIndex);
-  }
-
-  @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
-  }
-}
+// import 'dart:io';
+//
+// import 'package:fa_flutter_core/fa_flutter_core.dart';
+// import 'package:flutter/material.dart';
+// import 'package:qr_code_scanner_plus/qr_code_scanner_plus.dart';
+//
+// class QrCodeScanner extends StatefulWidget {
+//   const QrCodeScanner({Key? key, this.validationRegex, this.scannedValues})
+//       : super(key: key);
+//
+//   final String? validationRegex;
+//   final List<String>? scannedValues;
+//
+//   @override
+//   State<QrCodeScanner> createState() => _QrScannerState();
+// }
+//
+// class _QrScannerState extends State<QrCodeScanner> {
+//   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+//   Barcode? result;
+//   QRViewController? controller;
+//   String? parsedData;
+//   final upiRegx = RegExp(r'[a-zA-Z0-9.\-_]{2,256}@[a-zA-Z]{2,64}');
+//   RegExp? validationRegex;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     if (widget.validationRegex != null) {
+//       validationRegex = RegExp(widget.validationRegex!);
+//     }
+//   }
+//
+//   @override
+//   void reassemble() {
+//     super.reassemble();
+//     if (Platform.isAndroid) {
+//       controller!.pauseCamera();
+//     } else if (Platform.isIOS) {
+//       controller!.resumeCamera();
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         children: <Widget>[
+//           Expanded(
+//             flex: 10,
+//             child: QRView(
+//               key: qrKey,
+//               cameraFacing: CameraFacing.back,
+//               overlay: QrScannerOverlayShape(
+//                 borderRadius: 12,
+//                 borderColor: Colors.green,
+//                 borderWidth: 10,
+//                 cutOutSize: 350,
+//               ),
+//               onQRViewCreated: (controller) {
+//                 if (this.controller == null) {
+//                   setState(() {
+//                     this.controller = controller;
+//                   });
+//                 }
+//
+//                 controller.resumeCamera(); //BUG
+//
+//                 controller.scannedDataStream.listen((scanData) {
+//                   if (scanData.code != null) {
+//                     if (upiRegx.hasMatch(scanData.code!)) {
+//                       parsedData = _extractUPIID(scanData.code!);
+//                     } else {
+//                       parsedData = scanData.code!;
+//                     }
+//                     result = scanData;
+//                     setState(() {});
+//                     controller.stopCamera();
+//                   }
+//                 });
+//               },
+//             ),
+//           ),
+//           if (result != null)
+//             Expanded(
+//               flex: 1,
+//               child: Padding(
+//                 padding: const EdgeInsets.all(16.0),
+//                 child: Text(parsedData ?? ''),
+//               ),
+//             ),
+//           Expanded(
+//             flex: 2,
+//             child: Row(
+//               mainAxisAlignment: result != null
+//                   ? MainAxisAlignment.spaceAround
+//                   : MainAxisAlignment.center,
+//               children: [
+//                 IconButton(
+//                   onPressed: () {
+//                     ScaffoldMessenger.of(context).clearSnackBars();
+//                     if (result != null) {
+//                       setState(() {
+//                         result = null;
+//                       });
+//                       controller?.resumeCamera();
+//                     } else {
+//                       Navigator.pop(context);
+//                     }
+//                   },
+//                   color: Colors.red,
+//                   iconSize: 35,
+//                   icon: Icon(Icons.close_outlined),
+//                 ),
+//                 if (result != null)
+//                   IconButton(
+//                     onPressed: onPressSubmit,
+//                     color: Colors.green,
+//                     iconSize: 35,
+//                     icon: Icon(Icons.check_rounded),
+//                   ),
+//               ],
+//             ),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+//
+//   void onPressSubmit() {
+//     if (result != null) {
+//       String? dataToReturn;
+//
+//       if (widget.validationRegex != null &&
+//           validationRegex != null &&
+//           !validationRegex!.hasMatch(result!.code!)) {
+//         ScaffoldMessenger.of(context).clearSnackBars();
+//         ScaffoldMessenger.of(context).showSnackBar(
+//           SnackBar(
+//             content: Text(
+//               "Scanned QR code does not match the validation checks, Please try again.",
+//             ),
+//           ),
+//         );
+//         return;
+//       }
+//
+//       if (parsedData != null) {
+//         dataToReturn = parsedData;
+//       } else if (result!.code != null) {
+//         dataToReturn = result!.code!;
+//       } else {
+//         throw Exception("Scanned data is empty");
+//       }
+//
+//       //incase of unique scans.
+//       if (checkIfListIsNotEmpty(widget.scannedValues)) {
+//         if (widget.scannedValues!.contains(dataToReturn)) {
+//           ScaffoldMessenger.of(context).clearSnackBars();
+//           ScaffoldMessenger.of(context).showSnackBar(
+//             SnackBar(
+//               content: Text(
+//                 "You have already scanned this QR code.",
+//               ),
+//             ),
+//           );
+//           return;
+//         }
+//       }
+//
+//       Navigator.pop(context, dataToReturn);
+//     }
+//   }
+//
+//   String _extractUPIID(String scannedData) {
+//     final startIndex = scannedData.indexOf('?pa=') + 4;
+//     final endIndex = scannedData.indexOf('&');
+//     return scannedData.substring(startIndex, endIndex);
+//   }
+//
+//   @override
+//   void dispose() {
+//     controller?.dispose();
+//     super.dispose();
+//   }
+// }
